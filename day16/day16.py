16efc25256d79e16d3ad79003d6cdb92470cd591
Kali <kali@kali>
2021-12-16 20:13:13 +0100

    testing day16

--- day16/day16.py
+++ day16/day16.py
@@ -21,57 +21,48 @@ Last three zeros are ignored because of hexadecimal padding
 Thus the literal value in the packet is 011111100101 = 2021 in decimal
 """
 
+pyint = int
+
+def int(x, y = 10):
+    return pyint(''.join(x), y)
+
 with open('test_input', 'r') as f:
     data = f.readline()
 
-stream_len = len(data)*4
-stream = (bin(int(data, 16))[2:]).zfill(stream_len)
-print(f'{stream=}')
+stream = list(''.join(bin(int(b, 16))[2:].zfill(4) for b in data.strip()))
 
-def decode_type4_packet(stream):
-    unformatted = []
-    zero = False
-    factor = 0
-    while not zero:
-        tmp = stream[6+5*factor:11+5*factor]
-        print(f'{chr(factor + 65)} = {tmp}')
-        unformatted.append(str(tmp))
-        if tmp[0] == '0':
-            zero = True
-        factor += 1
-    print(f'{unformatted=}')
-    num = int(''.join([i[1:5] for i in unformatted]), 2)
-    print(f'{num=}')
-    padding = factor*5 % 4
-    return 6 + 5*factor + padding
+def parse_packet_stream(packet):
+    version = int(packet[:3], 2)
+    packet = packet[3:]
 
-while stream:
-    version = stream[0:3]
-    type_id = stream[3:6]
+    typeid = int(packet[:3], 2)
+    packet = packet[3:]
 
-    if type_id == '100':
-        print(f'{version=}')
-        end_of_packet = decode_type4_packet(stream)
-        stream = stream[end_of_packet:]
+    if typeid == 4:
+        data = []
+        while True:
+            prefix = packet.pop(0)
+            data += packet[:4]
+            packet = packet[4:]
+            if prefix == '0':
+                break
+        data = int(data, 2)
+        return (data, version, typeid)
     else:
-        print(f'{version=}')
-        length_type_id = stream[6]
-        if length_type_id == '0':
-            length_of_packets = int(stream[7:22], 2)
-            stream = stream[22:]
-            sub_stream = stream[:length_of_pgackets]
-            current_length = 0
-            print(f'{length_of_packets=}')
-            while True:
-                if current_length >= length_of_packets:
-                    break
-                print(f'{sub_stream=}')
-                end = decode_type4_packet(sub_stream) - 1
-                print(f'{end=}')
-                current_length += end # ! Wrong length is getting calculated
-                sub_stream = sub_stream[end:]
-                print(f'{current_length=}')
-            stream = stream[length_of_packets:] # ! Somehow wrong padding because of eg: padding is 0000000 why not only 3 000 to get a multiple of 4????
-                
-                
+        packets = []
+        if packet.pop(0) == '0':
+            length = int(packet[:15], 2)
+            packet = packet[15:]
+            sub = packet[:length]
+            packet = packet[length:]
+            packets.append(parse_packet_stream(sub))
+        else:
+            amount_packets = int(packet[:11], 2)
+            packet = packet[11:]
+            print(amount_packets)
+            for _ in range(amount_packets):
+                packets.append(parse_packet_stream(packet))
+        return (version, typeid, packets)
 
+a = parse_packet_stream(stream)
+print(a)
